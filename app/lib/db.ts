ba5a0fbf7172a204af94c777fda425612f9660e6
Loomeh <loombadoop@gmail.com>
2025-01-28 17:47:01 +0000

    update postgres stuff

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 
 if (!process.env.POSTGRES_DATABASE_URL) {
-  throw new Error('NEON_DATABASE_URL environment variable is not set');
+  throw new Error('POSTGRES_DATABASE_URL environment variable is not set');
 }
 
 const sql = neon(process.env.POSTGRES_DATABASE_URL);
